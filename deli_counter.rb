fd6b9d8bba48a6d7934e15147bb4051aca03c4a2
Chantel Lowe <chantellowe17@gmail.com>
2019-07-31 02:30:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
       puts "There is nobody waiting to be served!"
-  else
-    
+  elsif katz_deli.length >= 1
+    katz_deli.shift
   end
 end
